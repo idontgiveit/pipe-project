623cc8afa641ebef46171f2669eea4e5f7caad93
bkirillov <bkirillov@solanteq.com>
2023-10-06 17:44:00 +0300

    add add feature

--- src/main/java/ru/otus/pipeline/Controller.java
+++ src/main/java/ru/otus/pipeline/Controller.java
@@ -1,8 +1,10 @@
 package ru.otus.pipeline;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.otus.service.BusinessService;
 
@@ -15,5 +17,11 @@ public class Controller {
     @GetMapping(path = "/start", produces = "application/json")
     public String start() {
         return "{ \"state\"  : \"started\"}";
+    }
+
+    @GetMapping(path = "/add/{number}")
+    public HttpStatus add (@PathVariable long number) {
+        businessService.add(number);
+        return HttpStatus.OK;
     }
 }
---
+++ src/test/java/ru/otus/test/BusinessServiceTest.java
@@ -0,0 +1,16 @@
+package ru.otus.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.otus.service.BusinessService;
+
+public class BusinessServiceTest {
+
+    @Test
+    public void testAdd() {
+        BusinessService service = new BusinessService();
+        service.add(1L);
+        Assertions.assertEquals(2, service.list().size());
+    }
+
+}
